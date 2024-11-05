76dffad1b3499f6766c12dbf9d5212670a75ebf7
keerthanasiripuram <98584553+keerthanasiripuram@users.noreply.github.com>
2024-11-05 09:00:09 +0530

    Update App.js

--- eventscheduler/src/App.js
+++ eventscheduler/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Scheduler from './pages/Scheduler';
-import { ViewSchedule } from './pages/ViewSchedule';
+
 import { BrowserRouter, Routes, Switch, Route, useNavigate } from "react-router-dom"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Scheduler/>}/>
-      <Route path="/view-schedule" element={<ViewSchedule/>}/>
+      <Route path="/" element={<Scheduler/>}/>    
     </Routes>
     </BrowserRouter>
     
